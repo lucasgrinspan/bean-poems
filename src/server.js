82ab9e6fa32eb4d43128cdfabafdd4394b2f7028
Lucas Grinspan <lucasgrinspan@gmail.com>
2020-08-31 18:15:59 -0400

    change port number

--- src/server.js
+++ src/server.js
@@ -8,4 +8,4 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/404/index.html"));
 });
 
-app.listen(3000, () => console.log("Server started"));
+app.listen(5000, () => console.log("Server started"));
